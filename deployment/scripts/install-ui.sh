2266b8ddbf7477252a503b0dbe803b6bbb67230c
betejb <betejb@yahoo.com>
2023-06-26 22:47:31 +0300

    test

--- deployment/scripts/install-ui.sh
+++ deployment/scripts/install-ui.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+rm -rf /tmp/ui/public
 unzip /tmp/ui/stocks-app-ui-0.0.1-SNAPSHOT.jar -d /tmp/ui
 cp -pr /tmp/ui/public/*  /var/www/html/
 chown -R apache:apache /var/www/html/*
--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+ps -ef | grep java | grep stocks | xargs kill -15
+sleep 10
 java -jar /stocks-app-api/stocks-app-api-0.0.1-SNAPSHOT.jar &
