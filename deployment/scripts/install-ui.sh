7bb99d043baf9191fcba5e775937daab0cda9200
betejb <betejb@yahoo.com>
2023-06-28 12:34:54 +0300

    test

--- deployment/scripts/install-ui.sh
+++ deployment/scripts/install-ui.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-rm -rf /tmp/ui/public
+rm -rf /tmp/ui/public 2>/dev/null
+rm -rf /var/www/html/* 2>/dev/null
 unzip /tmp/ui/stocks-app-ui-0.0.1-SNAPSHOT.jar -d /tmp/ui
 cp -pr /tmp/ui/public/*  /var/www/html/
 chown -R apache:apache /var/www/html/*
