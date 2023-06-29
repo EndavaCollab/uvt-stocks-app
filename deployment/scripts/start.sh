07b914f87edc933fa4492c5327f39227f0fa090a
betejb <betejb@yahoo.com>
2023-06-29 09:42:29 +0300

    test

--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 fuser -k 8080/tcp
 sleep 10
-java -jar /stocks-app-api/stocks-app-api-0.0.1-SNAPSHOT.jar &
+nohup java -jar /stocks-app-api/stocks-app-api-0.0.1-SNAPSHOT.jar &
+echo "Deployment complete"
