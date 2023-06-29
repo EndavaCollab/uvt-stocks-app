10dcc47e76bc25294ebb16cb3a7c1b6c4f5a2a39
lucatronlk <alex.luka@hotmail.com>
2023-06-29 10:14:17 +0300

    try to fix the nohup exit

--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 fuser -k 8080/tcp
 sleep 10
-echo | nohup java -jar /stocks-app-api/stocks-app-api-0.0.1-SNAPSHOT.jar &
+nohup java -jar /stocks-app-api/stocks-app-api-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+disown
 echo "Deployment complete"
