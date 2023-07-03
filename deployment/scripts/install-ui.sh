431644d9f36c1f3396ad876883a6e8bfd1bb9f97
lucatronlk <alex.luka@hotmail.com>
2023-07-03 17:44:04 +0300

    beforeInstall.sh

--- deployment/scripts/install-ui.sh
+++ deployment/scripts/install-ui.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-
-# rm -rf /tmp/ui/public 2>/dev/null
-# rm -rf /var/www/html/* 2>/dev/null
+rm -rf /tmp/ui/public 2>/dev/null
+rm -rf /var/www/html/* 2>/dev/null
 
 unzip /tmp/ui/stocks-app-ui-0.0.1-SNAPSHOT.jar -d /tmp/ui
 cp -pr /tmp/ui/public/*  /var/www/html/
