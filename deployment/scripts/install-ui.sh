ed0d5ab5246ff58ac1e6e796682f0e93c438d50e
lucatronlk <alex.luka@hotmail.com>
2023-07-03 17:00:35 +0300

    test failed build

--- deployment/scripts/install-ui.sh
+++ deployment/scripts/install-ui.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 set -e
 
-rm -rf /tmp/ui/public 2>/dev/null
-rm -rf /var/www/html/* 2>/dev/null
+# rm -rf /tmp/ui/public 2>/dev/null
+# rm -rf /var/www/html/* 2>/dev/null
+
 unzip /tmp/ui/stocks-app-ui-0.0.1-SNAPSHOT.jar -d /tmp/ui
 cp -pr /tmp/ui/public/*  /var/www/html/
 chown -R apache:apache /var/www/html/*
