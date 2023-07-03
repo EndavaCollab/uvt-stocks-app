4efa6c74f15bec3d4bcf881597f2df2b8b098804
lucatronlk <alex.luka@hotmail.com>
2023-07-03 18:24:34 +0300

    add install ui sh

--- deployment/scripts/install-ui.sh
+++ deployment/scripts/install-ui.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-yum install zip
-zip -h
-unzip /tmp/ui/stocks-app-ui-0.0.1-SNAPSHOT.jar -d /tmp/ui 2>/dev/null
-cp -pr /tmp/ui/public/*  /var/www/html/ 2>/dev/null
-chown -R apache:apache /var/www/html/* 2>/dev/null
+unzip /tmp/ui/stocks-app-ui-0.0.1-SNAPSHOT.jar -d /tmp/ui
+cp -pr /tmp/ui/public/*  /var/www/html/
+chown -R apache:apache /var/www/html/*
